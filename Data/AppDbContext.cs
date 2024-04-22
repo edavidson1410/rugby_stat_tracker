2c0533c6a0eb45bdab4b12db98869f1e3a57f94c
edavidson1410 <edavidson1410@gmail.com>
2024-04-21 20:22:23 -0500

    Database setup progress

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using rugby_stat_tracker.Models;
+
+namespace rugby_stat_tracker.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("rugby_stat_tracker"));
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace rugby_stat_tracker.Models
+{
+    public class Player
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Birthdate { get; set; }
+        public string? Position { get; set; }
+        public string? Hometown { get; set; }
+
+    }
+}
