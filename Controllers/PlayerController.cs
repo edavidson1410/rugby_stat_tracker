e7cb11eada1e1bc81ee3b3201db5f5f72db388ab
edavidson1410 <edavidson1410@gmail.com>
2024-04-26 10:43:55 -0500

    Added GET requests to Player pipeline

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using rugby_stat_tracker.Models;
 
 namespace rugby_stat_tracker.Controllers
 {
-    [Route("api/[controller]")]
+    //generic api endpoint name, could use [controller] but would like endpoint to stay same even during refactoring.
+    [Route("api/players")]
     [ApiController]
-    public class PlayerController : Controller
+    public class PlayerController : ControllerBase
     {
         //GET all players
+        [HttpGet]
+        public ActionResult<IEnumerable<Player>> GetPlayers()
+        {
+            return Ok(PlayerDataStore.Current.Players);
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<Player> GetPlayer(int id)
+        {
+
+            var playerResult = PlayerDataStore.Current.Players
+                .FirstOrDefault(p => p.Id == id);
+
+            if (playerResult == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(playerResult);
+        }
 
     }
 }
---
+++ PlayerDataStore.cs
@@ -0,0 +1,44 @@
+﻿using rugby_stat_tracker.Models;
+
+namespace rugby_stat_tracker
+{
+    
+    public class PlayerDataStore
+    {
+        public List<Player> Players { get; set; }
+
+        public static PlayerDataStore Current { get; } = new PlayerDataStore();
+
+        public PlayerDataStore()
+        {
+            Players = new List<Player>()
+            {
+                new Player()
+                {
+                    Id = 1,
+                    Name = "Eric Davidson",
+                    Hometown = "Jackson, MO",
+                    Birthdate = DateTime.Parse("1992-07-26"),
+                    Position = "Centre"
+                },
+                new Player()
+                {
+                    Id = 2,
+                    Name = "Aidan Milne",
+                    Hometown = "Nelson, New Zealand",
+                    Birthdate = DateTime.Parse("1992-07-26"),
+                    Position = "Centre"
+                },
+                new Player()
+                {
+                    Id = 3,
+                    Name = "Victor Romero",
+                    Hometown = "Mexico",
+                    Birthdate = DateTime.Parse("1992-07-26"),
+                    Position = "Flanker"
+                }
+            };
+        }
+
+    }
+}
